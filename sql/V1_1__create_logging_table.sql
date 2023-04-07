9e66fa116487e2c37577b7ddd27635ac58ba3bd5
NkLapp <34657611+NkLapp@users.noreply.github.com>
2023-04-07 10:02:30 +0300

    created database migrations for logging bot location and actions

--- sql/V1_1__create_logging_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.dbot_logs (
-    id SERIAL PRIMARY KEY,
-    log_type INTEGER NOT NULL, 
-    log_action INTEGER,
-    log_text VARCHAR(255),
-    log_error VARCHAR(255),
-    log_location VARCHAR(255),
-    log_timestamp TIMESTAMP DEFAULT NOW()
-);
